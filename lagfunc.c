e4ee5a58f5775bca983b8d520f41e3cfb4e484cc
SFPhotography <fischer.simon@gmx.de>
2015-11-02 18:15:31 +0100

    Neues

---
+++ lagfunc.c
@@ -0,0 +1,23 @@
+/*
+ * lagfunc.c
+ *
+ *  Created on: 02.11.2015
+ *      Author: Simon
+ */
+#include <stdio.h>
+#include "lagdat.h"
+
+int bestellMenge()
+{
+
+}
+
+int bestellUebersicht()
+{
+
+}
+
+int bestellSort()
+{
+
+}
---
+++ main.c
@@ -0,0 +1,33 @@
+/*
+ * main.c
+ *
+ *  Created on: 02.11.2015
+ *      Author: Simon
+ */
+#include <stdio.h>
+#include "lagdat.h"
+
+int iRet=0;
+int gesamtzahl=0;
+float gesamtwert=0;
+
+int main (void)
+{
+	iRet=openLager; //Lager öffnen
+	if(iRet==-1)
+		printf("Die Lagerdatei konnte nicht geöffnet werden!"); //standartausgabe bei Fehler beim öffnen
+
+	while(iRet!=0) //bestimmung der gesamtzahl unterschiedlicher artikel und gesamtwert
+	{
+		gesamtwert=gesamtwert+(preis x bestand); //bestimmung des Gesamtwert durch multiplizieren von menge und preis
+		gesamtzahl++; //erhöhen der gesamtzahl der artikel
+		iRet=readnext(struct artikel_t); //iRet erneut setzen
+	}
+	printf("Gesamtanzahl der Artikel:%d",gesamtzahl); //standartausgabe der anzahl
+	printf("Gesamtwert aller Artikel: %f",gesamtwert); ///standartausgaber des Werts
+
+
+
+	return 0;
+}
+
